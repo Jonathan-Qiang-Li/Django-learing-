7b892eeeeaa7f5442a239aa7479106839d854023
Jonathan-Qiang-Li <60246227+Jonathan-Qiang-Li@users.noreply.github.com>
2020-08-24 17:26:12 -0400

    inital before part2

---
+++ django_project_1/blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+def home(Request):
+    return HttpResponse('<h1>Blog Home</h1>')
+
+
+def about(Request):
+    return HttpResponse('<h1>Blog About</h1>')
